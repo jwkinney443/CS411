133904c53b1f00006fa45c981b5c50c51453f5f8
jwkinney443 <johnkinney443@gmail.com>
2024-03-26 10:08:40 -0400

    Created a ticket master testing file for some basic API calls, searches event based on keyword, zip code, or both.

---
+++ tmaster.js
@@ -0,0 +1,115 @@
+//API key and API URL
+const apiKey = 'PUT API KEY HERE';
+const apiUrl = 'https://app.ticketmaster.com/discovery/v2/events.json';
+
+
+//This function finds the first event from a string keyword
+function searchEvents(keyword) {
+  const url = `${apiUrl}?keyword=${keyword}&apikey=${apiKey}`;
+
+  // Fetch 
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Response Error.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Filtering data section
+      // (can experiment with)
+      if (data._embedded && data._embedded.events && data._embedded.events.length > 0) {
+        const firstEvent = data._embedded.events[0];
+        console.log('First Event:', firstEvent);
+
+        if (firstEvent.url) {
+          console.log('Event URL:', firstEvent);
+        }
+        else {
+          console.log('Event URL not available');
+        }
+      } else {
+        console.log('No events found.')
+      }
+
+    })
+    .catch(error => {
+      console.error('There was a problem with fetch:', error);
+    });
+}
+
+//This function finds the first event from a string zipcode
+function zcSearchEvents(zipcode) {
+  const url = `${apiUrl}?postalCode=${zipcode}&apikey=${apiKey}`;
+
+  // Fetch 
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Response Error.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Filtering data section
+      // (can experiment with)
+      if (data._embedded && data._embedded.events && data._embedded.events.length > 0) {
+        const firstEvent = data._embedded.events[0];
+        console.log('First Event:', firstEvent);
+
+        if (firstEvent.url) {
+          console.log('Event URL:', firstEvent);
+        }
+        else {
+          console.log('Event URL not available');
+        }
+      } else {
+        console.log('No events found.')
+      }
+
+    })
+    .catch(error => {
+      console.error('There was a problem with fetch:', error);
+    });
+
+}
+
+//This function finds the first event from a string keyword AND zipcode
+function proximitySearchEvents(keyword, zipcode) {
+  const url = `${apiUrl}?keyword=${keyword}&postalCode=${zipcode}&apikey=${apiKey}`;
+
+  // Fetch 
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Response Error.');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Filtering data section
+      // (can experiment with)
+      if (data._embedded && data._embedded.events && data._embedded.events.length > 0) {
+        const firstEvent = data._embedded.events[0];
+        console.log('First Event:', firstEvent);
+
+        if (firstEvent.url) {
+          console.log('Event URL:', firstEvent);
+        }
+        else {
+          console.log('Event URL not available');
+        }
+      } else {
+        console.log('No events found.')
+      }
+
+    })
+    .catch(error => {
+      console.error('There was a problem with fetch:', error);
+    });
+
+}
+
+//Testing Section
+
+
